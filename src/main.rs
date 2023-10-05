6009c546b7c45000757d9ee56bb5f8db49456a8d
joe <apjoey914@gmail.com>
2023-10-05 13:42:52 -0400

    Add db dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_cors::Cors;
-use actix_web::{http, App, HttpServer};
+use actix_web::{App, HttpServer};
 use std::env;
 
 #[actix_web::main]
@@ -8,8 +8,7 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default()
             .allowed_origin(&env::var("DOMAIN").unwrap_or("localhost".to_string()))
             .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"]);
-        App::new()
-            .wrap(cors)
+        App::new().wrap(cors)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
