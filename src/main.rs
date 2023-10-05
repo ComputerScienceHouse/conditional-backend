2981b96a9a065b05de076bbb38ca0c4274327cc7
qelxiros <jeremy3141592@gmail.com>
2023-10-05 00:56:51 -0400

    initial actix httpserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin(&env::var("DOMAIN").unwrap_or("localhost".to_string()))
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"]);
+        App::new()
+            .wrap(cors)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
