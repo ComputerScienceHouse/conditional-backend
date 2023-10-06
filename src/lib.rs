39c647f33825c91628fe2ff2185708a7e79cc24e
joe <apjoey914@gmail.com>
2023-10-05 21:12:49 -0400

    Add schema definitions

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+/// Data structure for API routes and DB access
+pub mod schema {
+    /// Schema returned and consumed by the API
+    pub mod api;
+    /// Schema retrieved from and written to the DB
+    pub mod db;
+}
+
+/// Internal routes to access the conditional database, separated by table
+pub mod table;
