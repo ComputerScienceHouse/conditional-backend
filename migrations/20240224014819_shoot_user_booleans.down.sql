a8599acee0d2ea6567ebce97ca7b37ba7b6e26f1
Jeremy Smart <jeremy3141592@gmail.com>
2024-02-23 21:43:31 -0500

    :clown:

--- migrations/20240224014819_shoot_user_booleans.down.sql
+++ migrations/20240224014819_shoot_user_booleans.down.sql
@@ -1,3 +1,3 @@
 -- Add down migration script here
-ALTER TABLE "user" ADD COLUMN is_csh boolean GENERATED ALWAYS AS (ipa_unique_id IS NOT NULL);
+ALTER TABLE "user" ADD COLUMN is_csh boolean GENERATED ALWAYS AS ("user".ipa_unique_id IS NOT NULL) STORED;
 ALTER TABLE "user" ADD COLUMN is_intro boolean NOT NULL DEFAULT true;
