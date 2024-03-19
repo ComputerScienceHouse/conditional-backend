d01cee7ef1ba7b768e84f9aa1f91ba54bb28db2c
Cecilia Lau <cecilialau6776@gmail.com>
2024-03-18 20:29:03 -0400

    Fix down migrations

--- migrations/20240224213800_fix_coop_pkey.down.sql
+++ migrations/20240224213800_fix_coop_pkey.down.sql
@@ -2,6 +2,5 @@
 alter table coop add date date not null default CURRENT_DATE;
 update coop set date = (select date((year*10000+601)::TEXT));
 alter table coop drop column year;
-ALTER TABLE coop DROP CONSTRAINT coop_pk;
 ALTER TABLE coop ADD id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL;
-ALTER TABLE coop ADD CONSTRAINT coop_pkey PRIMARY KEY id;
+ALTER TABLE coop ADD CONSTRAINT coop_pkey PRIMARY KEY (id);
