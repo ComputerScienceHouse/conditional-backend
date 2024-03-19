a35e73753fd08f2a935cb1a4723e91e7c2f2e968
Cecilia Lau <cecilialau6776@gmail.com>
2024-03-18 21:18:35 -0400

    fix intro_data_pkey migration

--- migrations/20240226021009_fix_intro_data_pkey.up.sql
+++ migrations/20240226021009_fix_intro_data_pkey.up.sql
@@ -1,4 +1,8 @@
 -- Add up migration script here
-ALTER TABLE intro_eval_data DROP CONSTRAINT intro_eval_data_pkey;
+CREATE TABLE intro_eval_data_temp (LIKE intro_eval_data);
+INSERT INTO intro_eval_data_temp (id, uid, eval_block_id, social_events, other_comments, status)
+SELECT DISTINCT ON (uid, eval_block_id) id, uid, eval_block_id, social_events, other_comments, status FROM intro_eval_data;
+DROP TABLE intro_eval_data;
+ALTER TABLE intro_eval_data_temp RENAME TO intro_eval_data;
 ALTER TABLE intro_eval_data ADD CONSTRAINT intro_eval_data_pkey PRIMARY KEY (uid, eval_block_id);
-alter table intro_eval_data drop column id;
+ALTER TABLE intro_eval_data DROP COLUMN id;
